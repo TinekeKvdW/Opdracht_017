18ac81d79b3cf198d74261a9f750273b12ac5083
TinekeKvdW <84176956+TinekeKvdW@users.noreply.github.com>
2021-06-14 10:50:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculateSquared(number1, number2) {
+    const total = number1 * number1; number2 * number2;
+    const calculateSquared = total + total;
+    const sum = total * total;
+    return result;
+};
+console.log(result);
+/*
+calculateSquared(5, 8);
+
+const calculateSquared = function (number1, number2) {
+    const total = number1 * number1; number2 * number2;
+    const calculateSquared = total + total;
+    const sum = total * total;
+    return result;
+};
+
+calculateSquared(5, 8);
+
+const calculateSquared = (number1, number2) => {
+    const total = number1 * number1; number2 * number2;
+    const calculateSquared = total + total;
+    const sum = total * total;
+    return result;
+};
+calculateSquared(5, 8); */
